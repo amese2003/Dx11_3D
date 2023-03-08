117221b1656024a43cbf4e6b6c94dbffa5ba4c79
Yong Ju Kwak <amese1225@gmail.com>
2023-03-08 22:31:16 +0900

    텍스쳐

--- Dx11_3D/Engine/GeometryHelper.h
+++ Dx11_3D/Engine/GeometryHelper.h
@@ -5,12 +5,18 @@
 class GeometryHelper
 {
 public:
+	static void CreateQuad(shared_ptr<Geometry<VertexColorData>> geometry, Color color);
+
+	static void CreateQuad(shared_ptr<Geometry<VertexTextureData>> geometry);
+	static void CreateCube(shared_ptr<Geometry<VertexTextureData>> geometry);
+	static void CreateSphere(shared_ptr<Geometry<VertexTextureData>> geometry);
+	static void CreateGrid(shared_ptr<Geometry<VertexTextureData>> geometry, int32 sizeX, int32 sizeZ);
+
+public:
 	static void CreateRectangle(shared_ptr<Geometry<VertexColorData>> geometry, Color color);
 	static void CreateRectangle(shared_ptr<Geometry<VertexTextureData>> geometry);
 
-	static void CreateGrid(shared_ptr<Geometry<VertexTextureData>> geometry, int32 sizeX, int32 sizeZ);
-	static void CreateCube(shared_ptr<Geometry<VertexTextureData>> geometry);
-	static void CreateSphere(shared_ptr<Geometry<VertexTextureData>> geometry);
+	
 
 	static void CreateQuad(shared_ptr<Geometry<VertexTextureNormalData>> geometry);
 	static void CreateCube(shared_ptr<Geometry<VertexTextureNormalData>> geometry);
