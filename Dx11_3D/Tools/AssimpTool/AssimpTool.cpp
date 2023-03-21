25563af72ee7c863b1398b739ea543b0438ef932
용주 곽 <amese1225@gmail.com>
2023-03-21 16:52:57 +0900

    Bone Mesh로딩

--- Dx11_3D/Tools/AssimpTool/AssimpTool.cpp
+++ Dx11_3D/Tools/AssimpTool/AssimpTool.cpp
@@ -4,19 +4,34 @@
 
 void AssimpTool::Init()
 {
+	//{
+	//	shared_ptr<Converter> converter = make_shared<Converter>();
+
+	//	// FBX -> Memory
+	//	converter->ReadAssetFile(L"House/House.fbx");
+
+
+	//	// Memory -> CustomData
+	//	converter->ExportMaterialData(L"House/House");
+	//	converter->ExportModelData(L"House/House");
+
+	//	// CustomData -> Memory
+	//	
+	//}
+
 	{
 		shared_ptr<Converter> converter = make_shared<Converter>();
 
 		// FBX -> Memory
-		converter->ReadAssetFile(L"House/House.fbx");
+		converter->ReadAssetFile(L"Tower/Tower.fbx");
 
 
 		// Memory -> CustomData
-		converter->ExportMaterialData(L"House/House");
-		converter->ExportModelData(L"House/House");
+		converter->ExportMaterialData(L"Tower/Tower");
+		converter->ExportModelData(L"Tower/Tower");
 
 		// CustomData -> Memory
-		
+
 	}
 }
 
